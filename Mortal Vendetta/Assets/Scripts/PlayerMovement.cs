30330e6d3f8c3219a6811fa0be105c28a8d25dee
GodGivenn <balicabogdaniulian@gmail.com>
2022-08-09 20:17:23 +0300

    A lot of changes
    Player can now climb walls(still need an animation)
    Adjusted code(more efficent)
    Adding melee and ranged attacks

--- Mortal Vendetta/Assets/Scripts/PlayerMovement.cs
+++ Mortal Vendetta/Assets/Scripts/PlayerMovement.cs
@@ -3,10 +3,14 @@
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]private float speed;
-    [SerializeField]private float jump;
+    [SerializeField]private float jumpPower;
+    [SerializeField]private LayerMask groundLayer;
+    [SerializeField]private LayerMask wallLayer;
     private Rigidbody2D body;
     private Animator anim;
-    private bool grounded;
+    private BoxCollider2D boxCollider;
+    private float wallJumpCooldown;
+    private float horizontalInput;
 
 
     private void Awake()
@@ -14,13 +18,12 @@ public class PlayerMovement : MonoBehaviour
         ///grabs references from object
         body = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
     private void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        body.velocity = new Vector2(horizontalInput * speed,body.velocity.y);
-
+        horizontalInput = Input.GetAxis("Horizontal");
 
         ///Flips player when moving left-right
         if(horizontalInput > 0.01f) 
@@ -28,26 +31,67 @@ public class PlayerMovement : MonoBehaviour
         else if(horizontalInput < -0.01f)
             transform.localScale = new Vector3(-1,1,1);
 
-        if(Input.GetKey(KeyCode.Space) && grounded)
-            Jump();
-
         //animator parameters
         anim.SetBool("run", horizontalInput != 0);
-        anim.SetBool("grounded", grounded);
+        anim.SetBool("grounded", isGrounded());
+
+        //walljump
+        if(wallJumpCooldown > 0.2f)
+        {
+            body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
+            if(onWall() && !isGrounded())
+            {
+                body.gravityScale = 0;
+                body.velocity = Vector2.zero;
+            }
+            else 
+                body.gravityScale = 1.25f;
+        }
+        else wallJumpCooldown += Time.deltaTime;
+
+        if(Input.GetKey(KeyCode.Space))
+            Jump();
     }
 
     private void Jump()
     {
-        body.velocity = new Vector2(body.velocity.x, speed + jump);
-        anim.SetTrigger("jump");
-        grounded = false;
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(collision.gameObject.tag == "Ground")
-            grounded = true;
+        if(isGrounded())
+        {
+            body.velocity = new Vector2(body.velocity.x, jumpPower);
+            anim.SetTrigger("jump");
+        }
+        else if(onWall() && !isGrounded())
+        {
+            if(horizontalInput == 0)
+            {
+                body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 3, 0);
+                transform.localScale = new Vector3(-Mathf.Sign(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+            }
+            else
+                body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 1, 1.5f);
+            //Mathf.Sign => 1 when player faces right or -1 when it faces left
+            //the minus is so we push the player away from the wall
+            wallJumpCooldown = 0;
+        }
+        
     }
     
+    private bool isGrounded()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer); 
+        return raycastHit.collider != null;  //if the player is in the air, raycastHit.collider will return false => null != null
+    }
+
+    private bool onWall()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, new Vector2(transform.localScale.x, 0), 0.1f, wallLayer); 
+        return raycastHit.collider != null; 
+    }
+
+    public bool canAttack()
+    {
+        return horizontalInput == 0 && isGrounded() && !onWall(); 
+    }
 }
  
---
+++ Mortal Vendetta/Assets/Scripts/Projectile.cs
@@ -0,0 +1,24 @@
+﻿
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private bool hit;
+
+    private BoxCollider2D boxCollider;
+    private Animator anim;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    private void Update()
+    {
+        if(hit) return;
+        float movementSpeed = speed * Time.deltaTime;
+        transform.Translate(movementSpeed, 0, 0);
+    }
+}
